4a9a2319af71b0cfa550855ac04154f082bb1b9a
JonasArnold77 <105543936+JonasArnold77@users.noreply.github.com>
2026-03-21 14:01:41 +0100

    Healtsh SDK Added under Libs

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
-
+    implementation(files("libs/samsung-health-sensor-api-1.4.1.aar"))
     implementation("androidx.compose.ui:ui:1.7.2")
     implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.2")
